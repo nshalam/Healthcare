e6aeeb7dd6c28da3a7349e22f9f4cae542ba9872
nshalam <nathaniel.shalam25@trinityschoolnyc.org>
2024-04-24 14:15:44 -0400

    Project Progress

---
+++ data/reformatData.py
@@ -0,0 +1,21 @@
+import json
+import csv
+
+f1 = open("data/refined_data.csv", "r", encoding="utf-8-sig")
+lines = f1.readlines()
+
+headers = lines[0].strip().split(',')
+
+dictionary = []
+
+for line in lines[2:]:
+    values = line.strip().split(',')
+    row_dict = dict(zip(headers, values))
+    dictionary.append(row_dict)
+
+f1.close()
+
+f2 = open("data/refined_data.json", "w")
+json.dump(dictionary, f2, indent = 4)
+
+f2.close()
---
+++ server.py
@@ -0,0 +1,69 @@
+#flask --app data_server run
+from flask import Flask
+from flask import render_template
+from flask import request
+import json
+
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def index():
+    f = open("data/refined_data.json", "r")
+    data = json.load(f)
+    f.close()
+    states=[]
+    for state in data:
+        states.append(state["Location"])
+        
+    return render_template('index.html', states=sorted(states), data=data)
+
+@app.route('/about')
+def about():
+    f = open("data/refined_data.json", "r")
+    data = json.load(f)
+    f.close()
+    states=[]
+    for state in data:
+        states.append(state["Location"])
+
+    return render_template('about.html', states=sorted(states))
+
+@app.route('/state')
+def state():
+    location = request.args.get('name')
+    with open("data/refined_data.json", "r") as f:
+        data = json.load(f)
+    
+    states = set()
+    employer = nongroup = medicaid = medicare = military = uninsured = None
+
+    for state in data:
+        states.add(state['Location'])
+        if state['Location'].lower() == location.lower():
+            employer = state['Employer']
+            nongroup = state['Non-Group']
+            medicaid = state['Medicaid']
+            medicare = state['Medicare']
+            military = state['Military']
+            uninsured = state['Uninsured']
+
+    if employer is None:  # No matching state found
+        return "State not found", 404
+
+    return render_template('state.html', state=location, employer=float(employer), nongroup=float(nongroup), medicaid=float(medicaid), medicare=float(medicare), military=float(military), uninsured=float(uninsured), data=data, states=sorted(states))
+
+def navbar():
+    location = request.args.get('name')
+    f = open("data/refined_data.json", "r")
+    data = json.load(f)
+    f.close()
+    states=set()
+    for state in data:
+        states.add(state["Location"])
+    return render_template('navbar.html', state=location, data=data, states=sorted(states))
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
