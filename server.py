51023327cb258e04819ce191942c333e1e1cc95a
nshalam <nathaniel.shalam25@trinityschoolnyc.org>
2024-04-24 21:21:58 -0400

    Project Progress

--- server.py
+++ server.py
@@ -13,8 +13,10 @@ def index():
     data = json.load(f)
     f.close()
     states=[]
+    insured = {}
     for state in data:
         states.append(state["Location"])
+        insured[state["Abbreviation"]]=state["Uninsured"]
         
     return render_template('index.html', states=sorted(states), data=data)
 
@@ -26,7 +28,7 @@ def about():
     states=[]
     for state in data:
         states.append(state["Location"])
-
+        
     return render_template('about.html', states=sorted(states))
 
 @app.route('/state')
@@ -47,11 +49,12 @@ def state():
             medicare = state['Medicare']
             military = state['Military']
             uninsured = state['Uninsured']
+            population = state ['Population']
 
     if employer is None:  # No matching state found
         return "State not found", 404
 
-    return render_template('state.html', state=location, employer=float(employer), nongroup=float(nongroup), medicaid=float(medicaid), medicare=float(medicare), military=float(military), uninsured=float(uninsured), data=data, states=sorted(states))
+    return render_template('state.html', state=location, population=int(population), employer=float(employer), nongroup=float(nongroup), medicaid=float(medicaid), medicare=float(medicare), military=float(military), uninsured=float(uninsured), data=data, states=sorted(states))
 
 def navbar():
     location = request.args.get('name')
